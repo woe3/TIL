b73546f2b026a0d8507ecfb630270a20cbffedd0
hyunwoo <kimhyn21@gmail.com>
2022-11-15 13:02:01 +0900

    2022_11_15

---
+++ union_find.py
@@ -0,0 +1,30 @@
+import sys
+
+# 특정 원소가 속한 집합을 찾기(find)
+def find_parent(parent, x):
+    # 루트 노드를 찾을 때까지 재귀 호출
+    if parent[x] != x:
+        return find_parent(parent, parent[x])
+    return x
+
+# 두 원소가 속한 집합을 찾기(Union)
+def union_parent(parent, a, b):
+    a = find_parent(parent, a)
+    b = find_parent(parent, b)
+    if a < b:
+        parent[b] = a
+    else:
+        parent[a] = b
+
+# 노드의 개수와 간선의 개수 입력받기
+v, e = map(int, sys.stdin.readline.split())
+# 부모 테이블 초기화 하기
+parent = [0] * (v+1)
+# 부모 테이블상에서, 부모를 자기 자신으로 초기화
+for i in range(1, v+1):
+    parent[i] = i
+
+# Union 연산을 수행
+for i in range(e):
+    a, b = map(int, sys.stdin.readline.split())
+    union_parent(parent, a, b)
