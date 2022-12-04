ca218c82695d26c6654f7be2201027c6a5a60ac4
woe3 <116056778+woe3@users.noreply.github.com>
2022-12-04 14:00:20 +0900

    2022-12-03

--- cl/malloc_practice_2.c
+++ cl/malloc_practice_2.c
@@ -12,7 +12,11 @@ int main(void) {
     // pc가 가르키는 포인터를 1씩 증가시키며 알파벳 소문자를 삽입합니다.
     for(i = 0; i < 26; i++) {
         *(pc + i) = 'a'+i;
-        printf("%c", *(pc + i));
     }
+    //아스키 코드에서 0은 NULL를 의미한다.
+    *(pc + i) = 0;
+    printf("%d\n", i);
+    printf("%s\n", pc);
+    free(pc);
     return 0;
 }
--- test/test.py
+++ test/test.py
@@ -1,39 +1,5 @@
-import sys
-from collections import deque
-
-input = sys.stdin.readline
-n, k = map(int, input().split())
-virus = []
-new = [[] for _ in range(k + 1)] # k개 종류의 바이러스 존재
-
-dx = [-1, 1, 0, 0]
-dy = [0, 0, -1, 1]
-
-for i in range(n):
-    line = list(map(int, input().split()))
-    virus.append(line)
-    for j in range(n): # 처음 바이러스 상태 저장
-        if line[j]: new[line[j]].append((i, j))
-
-s, x, y = map(int, input().split())
-
-def bfs(k, new_virus):
-    queue = deque()
-    while new_virus:
-        queue.append(new_virus.pop())
-    while queue:
-        x, y = queue.popleft()
-        for i in range(4):
-            nx = x + dx[i]
-            ny = y + dy[i]
-            if nx < 0 or ny < 0 or nx >= n or ny >= n:  # 좌표 범위 유효성 판단
-                continue
-            if virus[nx][ny] == 0:
-                virus[nx][ny] = k
-                new_virus.append((nx, ny))
-
-for _ in range(s):
-    for i in range(1, k + 1):
-        bfs(i, new[i])
-
-print(virus[x-1][y-1])
+keyboard = [
+    ['q','w','e','r','t','y','u','i','o','p'],
+    ['a','s','d','f','g','h','j','k','l'],
+    ['z','x','c','v','b','n','m']    
+]
