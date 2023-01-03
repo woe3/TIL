c3984691e196bf32532c5c09b70f613416bbf2a1
woe3 <kimhyn21@gmail.com>
2023-01-03 21:20:16 +0900

    Update 72411_메뉴 리뉴얼.py

--- programers/72411_메뉴 리뉴얼.py
+++ programers/72411_메뉴 리뉴얼.py
@@ -1,6 +1,8 @@
+from itertools import combinations
+
 def solution(orders, course):
     answer_temp = [[] for _ in range(course[-1]+1)]
-    
+    checked_list = []
     length = len(orders)
     course_max = [0] * (course[-1]+1)
     for i in range(length):
@@ -8,30 +10,34 @@ def solution(orders, course):
         for j in range(i+1, length):
             menu_2 = set(orders[j])
             menu = menu_1 & menu_2
-            if len(menu) in course:
-                
+            if len(menu) >= course[0]:
                 menu = list(menu)
-                menu.sort()
-                menu_str = "".join(menu)
-                if menu_str in answer_temp[len(menu)]:
-                    continue
-                count = 0
-                for k in orders:
-                    count_if_in = 0
-                    for l in menu:
-                        if l in k:
-                            count_if_in += 1
+                for x in course:
+                    if len(menu) >= x:
+                        combi_list = combinations(menu, x)
+                    for y in combi_list:
+                        y = list(y)
+                        y.sort()
+                        menu_str = "".join(y)
+                        if menu_str in checked_list:
+                            continue
+                        count = 0
+                        for k in orders:
+                            count_if_in = 0
+                            for l in y:
+                                if l in k:
+                                    count_if_in += 1
 
-                    if count_if_in == len(menu):
-                        count += 1
+                            if count_if_in == len(y):
+                                count += 1
 
-                if count == course_max[len(menu)]:
-                    answer_temp[len(menu)].append(menu_str)
-                elif count > course_max[len(menu)]:
-                    course_max[len(menu)] = count
-                    answer_temp[len(menu)] = []
-                    answer_temp[len(menu)].append(menu_str)
-
+                        if count == course_max[len(y)]:
+                            answer_temp[len(y)].append(menu_str)
+                        elif count > course_max[len(y)]:
+                            course_max[len(y)] = count
+                            answer_temp[len(y)] = []
+                            answer_temp[len(y)].append(menu_str)
+                        checked_list.append(menu_str)
     answer = []
     for i in answer_temp:
         answer += i
