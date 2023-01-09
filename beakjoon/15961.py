03d825f8f3f6e34bf34d0fc59b216f560f35426c
woe3 <116056778+woe3@users.noreply.github.com>
2023-01-09 22:37:03 +0900

    Create 15961.py

---
+++ beakjoon/15961.py
@@ -0,0 +1,38 @@
+import sys
+from collections import deque
+
+n, d, k, c = map(int, sys.stdin.readline().split())
+
+sushi = []
+for _ in range(n):
+    sushi.append(int(sys.stdin.readline()))
+
+answer = 0
+temp_answer = 0
+cnt = deque()
+coupon = 1
+dishes = [0] * (d+1)
+for i in range(n+k):
+    if i >= n:
+        i -= n
+    dishes[sushi[i]] += 1
+    cnt.append(sushi[i])
+    if dishes[sushi[i]] == 1:
+        temp_answer += 1
+    if len(cnt) > k:
+        out = cnt.popleft()
+        dishes[out] -= 1
+        if dishes[out] == 0:
+            temp_answer -= 1
+    if dishes[c] == 0:
+        coupon = 1
+    else:
+        coupon = 0
+    if temp_answer+coupon > answer:
+        answer = temp_answer + coupon
+
+
+print(answer)
+
+#질문
+#함수 리턴은 어떻게 받는가
