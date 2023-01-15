130dcf45f604d3120feb94dbd7f45e15335fe6b6
woe3 <116056778+woe3@users.noreply.github.com>
2023-01-15 17:12:33 +0900

    working_on: 문자열 압축

---
+++ programers/60057_문자열압축.py
@@ -0,0 +1,11 @@
+from collections import deque
+
+def solution(s):
+    answer = 0
+    s = list(s)
+    for cut in range(len(s)//2):
+        i = cut * 2
+        while:
+            if s[i-cut:i] == s
+        
+    return answer
