58d46cf323dfeba6629f9bdfa3d78d44b2fc4131
woe3 <kimhyn21@gmail.com>
2023-01-16 23:32:31 +0900

    Create 14719_빗물.py

---
+++ beakjoon/14719_빗물.py
@@ -0,0 +1,22 @@
+import sys
+h, w = map(int, sys.stdin.readline().split())
+blocks = list(map(int, sys.stdin.readline().split()))
+
+
+visited = [0] * w
+answer = 0
+for height in range(h, 0, -1):
+    temp_list = []
+    for block_index in range(w):
+        if blocks[block_index] >= height and not visited[block_index]:
+            temp_list.append(block_index)
+        
+    if len(temp_list) > 1:
+        for i in range(len(temp_list)-1):
+            for j in range(temp_list[i], temp_list[i+1]):
+                answer += height - blocks[j]        
+                visited[j] = 1
+    print(temp_list)
+    print(answer)
+print(answer)
+        
