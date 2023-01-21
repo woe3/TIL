4e40d505b24619c88708bbfee1fd9693f6d00fb8
woe3 <116056778+woe3@users.noreply.github.com>
2023-01-22 06:38:54 +0900

    java_practice

---
+++ java_lecture/src/chap_01/_05_VariableNaming.java
@@ -0,0 +1,22 @@
+package chap_01;
+
+public class _05_VariableNaming {
+    public static void main(String[] args) {
+        /* 변수 이름 짓는 법
+         * 1. 저장할 값에 어울리는 이름
+         * 2. 밑줄(_), 문자(abc), 숫자(123) 사용 가능 (공백 사용 불가)
+         * 3. 밑줄 또는 문자로 시작 가능
+         * 4. 한 단어 또는 2개 이상 단어의 연속
+         * 5. 소문자로 시작, 각 단어의 시작 글자는 대문자 (첫 단어 제외)
+         * 6. 예약어 사용 불가 (public, static, void, int, double, float, ...)
+         */
+
+         //입국 신고서 (여행)
+         String nationality = "대한민국"; //국적
+         String firstName = "현우"; // 이름
+         String lastName = "김"; //성
+         String dataOfBirth = "1990.10.27"; // 생년월일
+         String residentialAddress = "hillton hotel"; //체류지
+
+    }
+}
---
+++ java_lecture/src/chap_01/_Quiz_01.java
@@ -0,0 +1,14 @@
+package chap_01;
+
+public class _Quiz_01 {
+    public static void main(String[] args) {
+        String busNumber = "상암08번";
+        int time = 3;
+        double distance = 1.2;
+
+        System.out.println(busNumber + " 버스");
+        System.out.println("약 " + time + "분 후 도착");
+        System.out.println("남은 거리 " + distance + "km");
+
+    }
+}
