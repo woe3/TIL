6a19a40c1578f513d2eb6420b37c9a15bb18afb5
woe3 <kimhyn21@gmail.com>
2023-01-25 00:54:29 +0900

    fix: 문제해결!

--- beakjoon/14267_회사문화1.py
+++ beakjoon/14267_회사문화1.py
@@ -1,78 +1,33 @@
-# import sys
-# import heapq
-
-# n, m = map(int, sys.stdin.readline().split())
-# boss = [0] + list(map(int, sys.stdin.readline().split()))
-# employee = [[] for _ in range(n+1)] #직원이 가지는 직속부하 그래프
-# score = [0] * (n+1) # 직원들의 칭찬 점수 기록
-# compliment = [] # 누가 칭찬을 받았는지 i, w의 리스트
-
-# for i in range(1, n+1):
-#     if boss[i] > 0:
-#         employee[boss[i]].append(i)
-
-# for _ in range(m):
-#     i, w = map(int, sys.stdin.readline().split())
-#     heapq.heappush(compliment, (i, w))
-    
-# def dfs (num, point = 0):
-
-#     if not employee[num]:
-#         return
-    
-#     for i in employee[num]:
-#         score[i] += point
-#         if i == compliment[0][0]:
-#             new_point = compliment[0][1]
-#             score[i] += new_point
-#             heapq.heappop(compliment)
-#             dfs(i, point + new_point)
-#         else:
-#             dfs(i, point)
-            
-        
-
-# dfs(1)
-
-# for i in range(1, n+1):
-#     print(score[i], end = " ")
-            
-            
-
-
 import sys
 import heapq
+# 재귀 깊이 최대로 만들기
+sys.setrecursionlimit(1000000)
 
 n, m = map(int, sys.stdin.readline().split())
 boss = [0] + list(map(int, sys.stdin.readline().split()))
-employee = [[] for _ in range(n+1)] #직원이 가지는 직속부하 그래프
+employee = [[0] for _ in range(n+1)] #직원이 가지는 직속부하 그래프
 score = [0] * (n+1) # 직원들의 칭찬 점수 기록
-compliment = [] # 누가 칭찬을 받았는지 i, w의 리스트
 
+# 직원의 직속 부하 그래프 만들기
 for i in range(1, n+1):
     if boss[i] > 0:
         employee[boss[i]].append(i)
 
+# 칭찬 받은 직원의 부하 그래프의 0번 인덱스에 칭찬 점수 기록
 for _ in range(m):
     i, w = map(int, sys.stdin.readline().split())
-    heapq.heappush(compliment, (i, w))
+    employee[i][0] += w
     
-def dfs (num, k, point = 0):
-    
-    if num == k[0]:
-        point += k[1]
-        score[num] += k[1]
-        if compliment:
-            k = heapq.heappop(compliment)
-        else:
-            k = [0, 0]
-    
-    for i in employee[num]:
-        score[i] += point
-        
-        dfs(i, k, point)
+# dfs로 탐색하면서 칭찬 점수가 있는 직원이 있다면 포인트를 더하고 dfs 진행
+def dfs (num = 1, point = 0):
+    if len(employee[num]) == 1:
+        return
+    for i in range(1, len(employee[num])):
+        new_num = employee[num][i]
+        score[new_num] += (point + employee[new_num][0])
+        dfs(new_num, point + employee[new_num][0])
 
-dfs(1, heapq.heappop(compliment))
+dfs()
 
 for i in range(1, n+1):
     print(score[i], end = " ")
