1cdc02f95512acf4d7eda757017820a78859cd73
woe3 <116056778+woe3@users.noreply.github.com>
2023-01-25 00:07:49 +0900

    Update 14267_회사문화1.py

--- beakjoon/14267_회사문화1.py
+++ beakjoon/14267_회사문화1.py
@@ -1,45 +1,78 @@
-import sys
-from collections import deque
+# import sys
+# import heapq
+
 # n, m = map(int, sys.stdin.readline().split())
 # boss = [0] + list(map(int, sys.stdin.readline().split()))
-# employee = [[] for _ in range(n+1)]
-# compliment = [0] * (n+1)
+# employee = [[] for _ in range(n+1)] #직원이 가지는 직속부하 그래프
+# score = [0] * (n+1) # 직원들의 칭찬 점수 기록
+# compliment = [] # 누가 칭찬을 받았는지 i, w의 리스트
+
 # for i in range(1, n+1):
 #     if boss[i] > 0:
 #         employee[boss[i]].append(i)
-    
+
 # for _ in range(m):
 #     i, w = map(int, sys.stdin.readline().split())
-#     number = deque([i])
+#     heapq.heappush(compliment, (i, w))
     
-#     while number:
-#         num = number.popleft()
-#         compliment[num] += w
-#         for k in employee[num]:
-#             number.append(k)
+# def dfs (num, point = 0):
+
+#     if not employee[num]:
+#         return
+    
+#     for i in employee[num]:
+#         score[i] += point
+#         if i == compliment[0][0]:
+#             new_point = compliment[0][1]
+#             score[i] += new_point
+#             heapq.heappop(compliment)
+#             dfs(i, point + new_point)
+#         else:
+#             dfs(i, point)
+            
+        
+
+# dfs(1)
 
 # for i in range(1, n+1):
-#     print(compliment[i], end = " ")
+#     print(score[i], end = " ")
             
             
 
+
+import sys
+import heapq
 
 n, m = map(int, sys.stdin.readline().split())
 boss = [0] + list(map(int, sys.stdin.readline().split()))
+employee = [[] for _ in range(n+1)] #직원이 가지는 직속부하 그래프
+score = [0] * (n+1) # 직원들의 칭찬 점수 기록
+compliment = [] # 누가 칭찬을 받았는지 i, w의 리스트
 
-compliment = [0] * (n+1)
-total_score = 0    
+for i in range(1, n+1):
+    if boss[i] > 0:
+        employee[boss[i]].append(i)
+
 for _ in range(m):
     i, w = map(int, sys.stdin.readline().split())
-    i = boss[i]
-    while i > 0:
-        compliment[i] -= w
-        i = boss[i]
-
-total_score = -compliment[1]
-for i in range(1, n+1):
-    compliment[i] += total_score
+    heapq.heappush(compliment, (i, w))
     
+def dfs (num, k, point = 0):
+    
+    if num == k[0]:
+        point += k[1]
+        score[num] += k[1]
+        if compliment:
+            k = heapq.heappop(compliment)
+        else:
+            k = [0, 0]
+    
+    for i in employee[num]:
+        score[i] += point
+        
+        dfs(i, k, point)
+
+dfs(1, heapq.heappop(compliment))
 
 for i in range(1, n+1):
-    print(compliment[i], end = " ")
+    print(score[i], end = " ")
