a99b21d56fc1f31c029320b94bc181c6373310a9
woe3 <116056778+woe3@users.noreply.github.com>
2023-01-28 21:57:30 +0900

    java_spring_practice

--- java_spring_lecture/hello_spring_project_name/src/main/java/hello_company_name/hello_spring_project_name/SpringConfig.java
+++ java_spring_lecture/hello_spring_project_name/src/main/java/hello_company_name/hello_spring_project_name/SpringConfig.java
@@ -1,21 +1,36 @@
 package hello_company_name.hello_spring_project_name;
 
+import hello_company_name.hello_spring_project_name.repository.JdbcMemberRepository;
 import hello_company_name.hello_spring_project_name.repository.MemberRepository;
 import hello_company_name.hello_spring_project_name.repository.MemoryMemberRepository;
 import hello_company_name.hello_spring_project_name.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class SpringConfig {
 
+    private DataSource dataSource;
+
+    @Autowired
+    public  SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
     @Bean
     public MemberService memberService() {
+
         return new MemberService(memberRepository());
     }
 
     @Bean
     public MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
+
+//        return new MemoryMemberRepository();
+        return new JdbcMemberRepository(dataSource);
+
     }
 }
---
+++ java_spring_lecture/hello_spring_project_name/src/main/java/hello_company_name/hello_spring_project_name/controller/MemberForm.java
@@ -0,0 +1,13 @@
+package hello_company_name.hello_spring_project_name.controller;
+
+public class MemberForm {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
