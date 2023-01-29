3363cb3130b3e94532947b9a6785bdb017550f02
woe3 <kimhyn21@gmail.com>
2023-01-29 12:52:15 +0900

    java_spring_practice

--- java_spring_lecture/hello_spring_project_name/src/main/java/hello_company_name/hello_spring_project_name/SpringConfig.java
+++ java_spring_lecture/hello_spring_project_name/src/main/java/hello_company_name/hello_spring_project_name/SpringConfig.java
@@ -1,23 +1,29 @@
 package hello_company_name.hello_spring_project_name;
 
-import hello_company_name.hello_spring_project_name.repository.JdbcMemberRepository;
-import hello_company_name.hello_spring_project_name.repository.MemberRepository;
-import hello_company_name.hello_spring_project_name.repository.MemoryMemberRepository;
+import hello_company_name.hello_spring_project_name.repository.*;
 import hello_company_name.hello_spring_project_name.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.persistence.Entity;
+import javax.persistence.EntityManager;
 import javax.sql.DataSource;
 
 @Configuration
 public class SpringConfig {
 
-    private DataSource dataSource;
+//    private DataSource dataSource;
+//
+//    @Autowired
+//    public  SpringConfig(DataSource dataSource) {
+//        this.dataSource = dataSource;
+//    }
+    private EntityManager em;
 
     @Autowired
-    public  SpringConfig(DataSource dataSource) {
-        this.dataSource = dataSource;
+    public SpringConfig(EntityManager em) {
+        this.em = em;
     }
 
     @Bean
@@ -30,7 +36,9 @@ public class SpringConfig {
     public MemberRepository memberRepository() {
 
 //        return new MemoryMemberRepository();
-        return new JdbcMemberRepository(dataSource);
-
+//        return new JdbcMemberRepository(dataSource);
+//        return new JdbcTemplateRepository(dataSource);
+        return new JpaMemberRepository(em);
     }
+
 }
