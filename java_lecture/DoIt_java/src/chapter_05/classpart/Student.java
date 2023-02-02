ae42de4d1f745b6870f04865f8fb926d052a39cb
woe3 <kimhyn21@gmail.com>
2023-02-03 01:05:25 +0900

    study

--- java_lecture/DoIt_java/src/chapter_05/classpart/Student.java
+++ java_lecture/DoIt_java/src/chapter_05/classpart/Student.java
@@ -1,4 +1,13 @@
 package chapter_05.classpart;
 
 public class Student {
+
+    int studentID;
+    String studentName;
+    int grade;
+    String address;
+
+    public void showStudentInfo() {
+        System.out.println(studentName + ", " + address);
+    }
 }
---
+++ java_lecture/DoIt_java/src/chapter_05/classpart/StudentTest.java
@@ -0,0 +1,11 @@
+package chapter_05.classpart;
+
+public class StudentTest {
+    public static void main(String[] args) {
+        Student studentLee = new Student();
+        studentLee.studentName = "이순신";
+        studentLee.address = "서울시 서초구 서초동";
+
+        studentLee.showStudentInfo();
+    }
+}
